fc44ba4d4190d297c1c431c70b21b1a338179090
Sayemahamed <sayemahamed183@gmail.com>
2025-02-08 18:07:20 +0600

    added Linked in Bot to Job portal app using hardlink

--- Web/Job_Portal/LinkedInBot.py
+++ Web/Job_Portal/LinkedInBot.py
@@ -5,6 +5,7 @@ from html_to_markdown import convert_to_markdown
 import re
 
 
+
 def get_job_description(job_id: str) -> str:
     with sync_playwright() as p:
         browser: Browser = p.chromium.connect_over_cdp(
@@ -16,13 +17,36 @@ def get_job_description(job_id: str) -> str:
         page.wait_for_selector(selector="#job-details", timeout=15000)
         job_id_element = page.query_selector(selector="#job-details")
         details: str | None = convert_to_markdown(
-            "<div>" + job_id_element.inner_html() + "</div>"# type: ignore
-        )  
+            "<div>" + job_id_element.inner_html() + "</div>"  # type: ignore
+        )
         details = re.sub(r" +", " ", details)
         details = re.sub(r"\n+", "\n", details)
         return details if details else "No description found"
 
 
+def get_job_ids(job_type: str, job_count: int) -> list[str]:
+    with sync_playwright() as p:
+        browser: Browser = p.chromium.connect_over_cdp(
+            endpoint_url="http://localhost:9222/", timeout=5000
+        )
+        url: str = f"https://www.linkedin.com/jobs/{job_type}"
+        page: Page = browser.contexts[0].pages[1]
+        job_ids = []
+        for i in range(0, job_count, 25):
+            page.goto(url + f"?start={i}" if i != 0 else url, timeout=15000)
+            page.wait_for_selector(selector="li[data-occludable-job-id]", timeout=15000)
+            job_id_element = page.query_selector_all(
+                selector="li[data-occludable-job-id]"
+            )
+
+            for element in job_id_element:
+                job_ids.append(element.get_attribute(name="data-occludable-job-id"))
+        return job_ids
+
+
+# Example usage
+if __name__ == "__main__":
+    print(get_job_ids(job_type="sales", job_count=50))
 if __name__ == "__main__":
     job_ids = ["4129403317", "4125562922", "4144858273", "4129297008"]
     for job_id in job_ids:
--- Web/Job_Portal/tasks.py
+++ Web/Job_Portal/tasks.py
@@ -1,11 +1,9 @@
 from celery import shared_task
-import sys
 from rich import print
 from playwright.sync_api import sync_playwright
 from playwright.sync_api._generated import Browser, Page
 from html_to_markdown import convert_to_markdown
 import re
-
 
 @shared_task
 def get_job_description(job_id: str) -> str:
@@ -14,13 +12,13 @@ def get_job_description(job_id: str) -> str:
             endpoint_url="http://localhost:9222/", timeout=5000
         )
         url: str = f"https://www.linkedin.com/jobs/view/{job_id}"
-        page: Page = browser.contexts[0].pages[2]
+        page: Page = browser.contexts[0].pages[3]
         page.goto(url=url, timeout=20000)
         page.wait_for_selector(selector="#job-details", timeout=15000)
         job_id_element = page.query_selector(selector="#job-details")
         details: str | None = convert_to_markdown(
-            "<div>" + job_id_element.inner_html() + "</div>"# type: ignore
-        )  
+            "<div>" + job_id_element.inner_html() + "</div>"  # type: ignore
+        )
         details = re.sub(r" +", " ", details)
         details = re.sub(r"\n+", "\n", details)
         return details if details else "No description found"
@@ -29,4 +27,3 @@ def get_job_description(job_id: str) -> str:
 @shared_task
 def add():
     return "Task executed! without delay"
-
